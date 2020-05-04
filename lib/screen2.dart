f0bc10160017f7397dc96d5722cf1886d7d0bc2f
Víctor Córdova <theslapboy@gmail.com>
2020-05-04 15:46:16 -0700

    Implenting the second screen

---
+++ lib/screen2.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Screen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.blueGrey[300],
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
