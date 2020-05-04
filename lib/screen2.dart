bd638e0c1b96b5d010ef2513be89364a77e6b256
Víctor Córdova <theslapboy@gmail.com>
2020-05-04 16:35:36 -0700

    Implenting the favorite icon

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:petui/configuration.dart';
 
 class Screen2 extends StatelessWidget {
   @override
@@ -20,6 +21,63 @@ class Screen2 extends StatelessWidget {
                   ),
                 )
               ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 40),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                  IconButton(icon: Icon(Icons.share), onPressed: () {})
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Image.asset('iamges/pet-cat2.png'),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 120,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: shadowList,
+                borderRadius: BorderRadius.circular(20)
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 150,
+              child: Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: primaryGreen,
+                    child: Icon(Icons.favorite_border),
+                  ),
+                  Container()
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40),)
+              ),
             ),
           )
         ],
