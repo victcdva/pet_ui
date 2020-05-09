8b5887798c3de8ad93c487eac21b5e46262fb159
Víctor Córdova <theslapboy@gmail.com>
2020-05-09 10:57:41 -0700

    Adding the setting and logout option

--- lib/drawerScreen.dart
+++ lib/drawerScreen.dart
@@ -11,7 +11,38 @@ class _DrawerScreenState extends State<DrawerScreen> {
   Widget build(BuildContext context) {
     return Container(
       color: primaryGreen,
-      
+      padding: EdgeInsets.only(top: 50),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(),
+              SizedBox(width: 10,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('FirstName LastName', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text('Status', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
+                ],
+              )
+            ],
+          ),
+          Column(
+            
+          ),
+          Row(
+            children: [
+              Icon(Icons.settings, color: Colors.white,),
+              SizedBox(width: 10,),
+              Text('Settings', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+              SizedBox(width: 10,),
+              Container(width: 2, height: 20, color: Colors.white,),
+              SizedBox(width: 10,),
+              Text('Log out', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
